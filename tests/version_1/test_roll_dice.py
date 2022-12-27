d026c2dc5b2165a7aa6beb1bd042d3531d7500b9
Jason Christopher <jchristopher2448@gmail.com>
2022-12-27 14:14:45 -0600

    roll dice tests passed

--- tests/version_1/test_roll_dice.py
+++ tests/version_1/test_roll_dice.py
@@ -11,55 +11,72 @@ from ten_thousand.game_logic import GameLogic
 
 pytestmark = [pytest.mark.version_1]
 
-
-@pytest.mark.skip("TODO")
-def test_1_dice():
-    values = GameLogic.roll_dice(1)
-    assert len(values) == 1
-    value = values[0]
-    assert 1 <= value <= 6
-
-
-@pytest.mark.skip("TODO")
-def test_2_dice():
-    values = GameLogic.roll_dice(2)
-    assert len(values) == 2
-
-    for value in values:
+@pytest.mark.parametrize(
+    "num_dice,expected_length",
+    [
+        (1, 1),
+        (2, 2),
+        (3, 3),
+        (4, 4),
+        (5, 5),
+        (6, 6),
+    ],
+)
+def test_all_valid_dice_rolls(num_dice, expected_length):
+    roll = GameLogic.roll_dice(num_dice)
+    assert len(roll) == expected_length
+    for value in roll:
         assert 1 <= value <= 6
 
 
-@pytest.mark.skip("TODO")
-def test_3_dice():
-    values = GameLogic.roll_dice(3)
-    assert len(values) == 3
-
-    for value in values:
-        assert 1 <= value <= 6
-
-
-@pytest.mark.skip("TODO")
-def test_4_dice():
-    values = GameLogic.roll_dice(4)
-    assert len(values) == 4
-
-    for value in values:
-        assert 1 <= value <= 6
-
-
-@pytest.mark.skip("TODO")
-def test_5_dice():
-    values = GameLogic.roll_dice(5)
-    assert len(values) == 5
-
-    for value in values:
-        assert 1 <= value <= 6
-
-
-@pytest.mark.skip("TODO")
-def test_6_dice():
-    values = GameLogic.roll_dice(6)
-    assert len(values) == 6
-
-    for value in values:
-        assert 1 <= value <= 6
+# # @pytest.mark.skip("TODO")
+# def test_1_dice():
+#     values = GameLogic.roll_dice(1)
+#     assert len(values) == 1
+#     value = values[0]
+#     assert 1 <= value <= 6
+#
+#
+# @pytest.mark.skip("TODO")
+# def test_2_dice():
+#     values = GameLogic.roll_dice(2)
+#     assert len(values) == 2
+#
+#     for value in values:
+#         assert 1 <= value <= 6
+#
+#
+# @pytest.mark.skip("TODO")
+# def test_3_dice():
+#     values = GameLogic.roll_dice(3)
+#     assert len(values) == 3
+#
+#     for value in values:
+#         assert 1 <= value <= 6
+#
+#
+# @pytest.mark.skip("TODO")
+# def test_4_dice():
+#     values = GameLogic.roll_dice(4)
+#     assert len(values) == 4
+#
+#     for value in values:
+#         assert 1 <= value <= 6
+#
+#
+# @pytest.mark.skip("TODO")
+# def test_5_dice():
+#     values = GameLogic.roll_dice(5)
+#     assert len(values) == 5
+#
+#     for value in values:
+#         assert 1 <= value <= 6
+#
+#
+# @pytest.mark.skip("TODO")
+# def test_6_dice():
+#     values = GameLogic.roll_dice(6)
+#     assert len(values) == 6
+#
+#     for value in values:
+#         assert 1 <= value <= 6
